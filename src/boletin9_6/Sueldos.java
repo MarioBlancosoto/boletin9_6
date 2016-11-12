8a9bd5e2ef8041a454f038feb765f50568c71fe3
Acer <Acer@192.168.0.17>
2016-11-12 20:17:34 +0100

    dd

---
+++ src/boletin9_6/Sueldos.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9_6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Acer
+ */
+public class Sueldos {
+    
+    public void sueldos(){
+        int trabTotales;
+        int sueldo;
+        int trabmas1000=0;
+        int trabmenos1000=0;
+        
+        
+       do{
+        
+                  do{
+                      sueldo = Integer.parseInt(JOptionPane.showInputDialog("Inserte el sueldo de los trabajadores,no se permiten números negativos "));
+                }while(sueldo<0);
+                  
+        
+            
+          
+            if(sueldo>=1000&&sueldo<=1750){
+           trabmas1000++;
+                
+             
+                       
+             }if (sueldo>0&&sueldo<1000){
+                trabmenos1000++;   }
+             System.out.println(" os traballadores de mais de 1000 e son "+trabmas1000+" e os traballadores "
+                     + " de menos de 1000 son "+trabmenos1000);
+            
+        }
+        while(sueldo!=0);
+       
+  }
+      
+    }
+
+
